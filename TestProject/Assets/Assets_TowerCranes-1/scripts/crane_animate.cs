4ad49d81e69cd3b6324b7e98ac83fa94f310b7d3
Inseok Yoon <yoon92411@snu.ac.kr>
2021-08-27 16:45:49 +0900

    tower crane  obj test

---
+++ TestProject/Assets/Assets_TowerCranes-1/scripts/crane_animate.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crane_animate : MonoBehaviour
+{
+    public Animator animator;
+    public AnimatorControllerParameter animParam;
+
+	public float rotateYaw;
+    public float weight;
+    public float dolly;
+    public float hook;
+
+    public bool demoMode = false;
+
+    float randomYawIncrease;
+    float randomDollyIncrease;
+    float randomHookIncrease;
+
+    void Start()
+    {
+        randomYawIncrease = Random.Range( 1,10 );
+        randomDollyIncrease = Random.Range(0.1f,1.0f);
+        randomHookIncrease = Random.Range(0.1f,1.0f);
+    }
+
+	void Update ()
+    {
+        if ( demoMode )
+        {
+            rotateYaw += Time.deltaTime * randomYawIncrease;
+            dolly = ((Mathf.Sin( Time.time * randomDollyIncrease ) * 100) + 100) /2.0f;
+            hook = ((Mathf.Sin( Time.time * randomHookIncrease ) * 100) + 100) / 2.0f;
+        }
+
+        animator.SetFloat( "Rotate_YAW", Mathf.Abs( rotateYaw ) % 360  );
+        animator.SetFloat( "weight", weight );
+        animator.SetFloat( "dolly", dolly );
+        animator.SetFloat( "hook", hook );
+	}
+}
---
+++ TestProject/Assets/Assets_TowerCranes-1/scripts/crane_animate2.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crane_animate2 : MonoBehaviour
+{
+    public Animator animator;
+    public AnimatorControllerParameter animParam;
+
+	public float rotateYaw;
+    public float pitch;
+    public float hook;
+
+    public bool demoMode = false;
+
+    float randomYawIncrease;
+    float randomPitchIncrease;
+    float randomHookIncrease;
+
+    void Start()
+    {
+        randomYawIncrease = Random.Range( 1,10 );
+        randomPitchIncrease = Random.Range(0.1f,1.0f);
+        randomHookIncrease = Random.Range(0.1f,1.0f);
+    }
+
+	void Update ()
+    {
+        if ( demoMode )
+        {
+            rotateYaw += Time.deltaTime * randomYawIncrease;
+            pitch = ((Mathf.Sin( Time.time * randomPitchIncrease ) * 100) + 100) /2.0f;
+            hook = ((Mathf.Sin( Time.time * randomHookIncrease ) * 100) + 100) / 2.0f;
+        }
+
+        animator.SetFloat( "Rotate_YAW", Mathf.Abs( rotateYaw ) % 360  );
+        animator.SetFloat( "pitch", pitch );
+        animator.SetFloat( "hook", hook );
+	}
+}
